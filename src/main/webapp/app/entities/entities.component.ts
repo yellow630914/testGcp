6301df5a146b490ddd537a668231c4d9828562f5
yellow630914 <poopoo630914@cht.com.tw>
2023-06-19 17:34:50 +0800

    0619

--- src/main/webapp/app/entities/entities.component.ts
+++ src/main/webapp/app/entities/entities.component.ts
@@ -1,6 +1,14 @@
 import { defineComponent, provide } from 'vue';
 
 import UserService from '@/entities/user/user.service';
+import CountryService from './country/country.service';
+import DepartmentService from './department/department.service';
+import EmployeeService from './employee/employee.service';
+import JobService from './job/job.service';
+import JobHistoryService from './job-history/job-history.service';
+import LocationService from './location/location.service';
+import RegionService from './region/region.service';
+import TaskService from './task/task.service';
 // jhipster-needle-add-entity-service-to-entities-component-import - JHipster will import entities services here
 
 export default defineComponent({
@@ -8,6 +16,14 @@ export default defineComponent({
   name: 'Entities',
   setup() {
     provide('userService', () => new UserService());
+    provide('countryService', () => new CountryService());
+    provide('departmentService', () => new DepartmentService());
+    provide('employeeService', () => new EmployeeService());
+    provide('jobService', () => new JobService());
+    provide('jobHistoryService', () => new JobHistoryService());
+    provide('locationService', () => new LocationService());
+    provide('regionService', () => new RegionService());
+    provide('taskService', () => new TaskService());
     // jhipster-needle-add-entity-service-to-entities-component - JHipster will import entities services here
   },
 });
